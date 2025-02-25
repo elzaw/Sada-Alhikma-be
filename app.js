ad6a26f643f0189b78568e22516ce3b5a42582db
Mahmoud Elzaw <hoodaelzaw81@gmail.com>
2025-02-25 22:44:30 +0200

    vercel

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const accommodationRoutes = require("./Routes/AccommodationRoutes");
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 3000;
 
 connect(process.env.MONGO_URI)
   .then(() => {
