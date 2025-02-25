c0111cb1bf4cdbd8eb51537e015a07bf8952a444
Mahmoud Elzaw <hoodaelzaw81@gmail.com>
2025-02-26 00:53:04 +0200

    updates

--- Controllers/ClientController.js
+++ Controllers/ClientController.js
@@ -2,6 +2,15 @@ const { Client } = require("../Models/ClientModel.js");
 const { Client: WhatsAppClient, LocalAuth } = require("whatsapp-web.js");
 const qrcode = require("qrcode");
 
+// Initialize WhatsApp Client
+const whatsappClient = new WhatsAppClient({
+  authStrategy: new LocalAuth(), // Persist session data locally
+  puppeteer: {
+    headless: true, // Run in headless mode
+    args: ["--no-sandbox", "--disable-setuid-sandbox"], // Add these args for Linux environments
+  },
+});
+
 whatsappClient.on("qr", (qr) => {
   console.log("Scan this QR code with WhatsApp:");
   qrcode.toString(qr, { type: "terminal" }, (err, url) => {
