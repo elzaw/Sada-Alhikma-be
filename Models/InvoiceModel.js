951c92c7c923dda1b224d644bc6be5872e6777bb
Mahmoud Elzaw <hoodaelzaw81@gmail.com>
2025-02-26 21:51:06 +0200

    invoice

--- Models/InvoiceModel.js
+++ Models/InvoiceModel.js
@@ -12,12 +12,30 @@ const invoiceSchema = new mongoose.Schema(
     costPerPerson: { type: Number, required: true }, // تكلفة الفرد
     totalAmount: { type: Number, required: true }, // الإجمالي
     paidAmount: { type: Number, default: 0 }, // المدفوع
+    bankTransfer: { type: Number, default: 0 }, // التحويل البنكي
     remainingAmount: { type: Number, default: 0 }, // المتبقي
     paymentMethod: {
       type: String,
       enum: ["cash", "bankTransfer"],
       required: true,
     }, // طريقة الدفع
+    tripOption: {
+      type: String,
+      enum: [
+        "oneWay",
+        "roundTrip",
+        "makkah",
+        "makkahMadinah",
+        "returnOnly",
+        "accommodationOnly",
+      ],
+      required: true,
+    }, // خيارات الرحلة
+    madinahDepartureDate: { type: Date }, // تاريخ الذهاب إلى المدينة
+    madinahReturnDate: { type: Date }, // تاريخ العودة من المدينة
+    pickupLocation: { type: String, required: true }, // مكان الركوب
+    numberOfDays: { type: Number, required: true }, // عدد الأيام
+    reservationOfficer: { type: String, required: true }, // اسم مسجل الحجز
     notes: { type: String }, // ملاحظات
     createdBy: {
       type: mongoose.Schema.Types.ObjectId,
